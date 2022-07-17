685092862981f363d994752d3b24e76bb3b2d164
clebemachado <clebmachado.em@gmail.com>
2022-07-16 23:53:06 -0300

    Adicionado MusicaDTO searcher

--- src/main/java/br/com/javafy/dto/spotify/MusicaFullDTO.java
+++ src/main/java/br/com/javafy/dto/spotify/MusicaFullDTO.java
@@ -1,5 +1,6 @@
 package br.com.javafy.dto.spotify;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,6 +11,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MusicaDTO {
 
     @JsonProperty("id")
