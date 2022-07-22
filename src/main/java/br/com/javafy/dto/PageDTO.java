839a111a17b7ca4c7e854d54fcc5c5fad4fc8bc0
cassiafg <cassiafguimaraes@gmail.com>
2022-07-22 16:59:09 -0300

    Adicionado PageDTO

---
+++ src/main/java/br/com/javafy/dto/PageDTO.java
@@ -0,0 +1,18 @@
+package br.com.javafy.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class PageDTO<T> {
+    private Long totalElements;
+    private Integer totalPages;
+    private Integer page;
+    private Integer size;
+    private List<T> content;
+}
