d9787c6db0467dbd72c87c08890cd228a003d304
cassiafg <cassiafguimaraes@gmail.com>
2022-07-22 15:57:18 -0300

    Alterado ComentarioDTO

--- src/main/java/br/com/javafy/dto/ComentarioDTO.java
+++ src/main/java/br/com/javafy/dto/ComentarioDTO.java
@@ -5,21 +5,10 @@ import lombok.*;
 
 import javax.validation.constraints.Size;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode
-@ToString
-public class ComentarioDTO {
+@Data
+public class ComentarioDTO extends ComentarioCreateDTO {
 
-    @Schema(description = "identificador unico do comentario")
+    @Schema(description = "Identificador unico do comentario")
     private Integer idComentario;
-    @Schema(description = "identificador unico da playlist")
-    private Integer idPlaylist;
-    @Schema(description = "identificador unico do usuario")
-    private Integer idUser;
-    @Schema(description = "comentario sobre a playlist")
-    @Size(max = 255)
-    private String comentario;
+
 }
