4bf8e5cc14a7aac59637f3d4ed36f73422320a7a
RodrigoSoaresNascimento <rodrigosnascimento1@gmail.com>
2022-08-06 16:59:53 -0300

    restaurando configurações

--- src/main/java/br/com/javafy/entity/TesteAlbum.java
+++ src/main/java/br/com/javafy/entity/TesteAlbum.java
@@ -1,7 +1,7 @@
 package br.com.javafy.entity;
 
-//import org.springframework.data.mongodb.core.mapping.Document;
-//
-//@Document
-//public class TesteAlbum {
-//}
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class TesteAlbum {
+}
