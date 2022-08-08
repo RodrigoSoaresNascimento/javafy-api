1af236eb9939dac4d220da32696b390461e3cef9
clebemachado <clebmachado.em@gmail.com>
2022-08-08 09:05:45 -0300

    Atualizado algumas informações do POSTs

--- src/main/java/br/com/javafy/service/PostsService.java
+++ src/main/java/br/com/javafy/service/PostsService.java
@@ -2,6 +2,7 @@ package br.com.javafy.service;
 
 import br.com.javafy.dto.posts.PostsCreateDTO;
 import br.com.javafy.dto.posts.PostsDTO;
+import br.com.javafy.entity.ComentsEntity;
 import br.com.javafy.entity.PostsEntity;
 import br.com.javafy.entity.UsuarioEntity;
 import br.com.javafy.exceptions.PessoaException;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -39,5 +41,7 @@ public class PostsService {
                 .map(postsEntity -> objectMapper.convertValue(postsEntity, PostsDTO.class))
                 .toList();
     }
+
+
 }
 
